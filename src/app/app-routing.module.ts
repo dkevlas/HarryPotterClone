5f31e6097156c784dd18faba4784e8fa942a2438
Kevin <dennisk.blas@gmail.com>
2024-10-21 16:20:53 -0500

    Routing para Not Found y Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./components/home/home.module').then(module => module.HomeModule)
+  },
+  {
+    path: 'inicio',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./error/error.module').then(module => module.ErrorModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
