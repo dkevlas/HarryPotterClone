d4486cc1bfcd01d38f7b23125f3c5761634657a7
Kevin <dennisk.blas@gmail.com>
2024-10-22 18:09:59 -0500

    Lógica para cambio de posicion de logos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { positions } from './../../data/houses';
+import { AfterViewInit, Component } from '@angular/core';
+import { allHouse, House } from '../../data/houses';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
 
+  ngAfterViewInit(): void {
+  }
+  houses: House[] = allHouse
+
+  areas = ['a', 'b', 'c', 'd']
+  gridArea(index: number){
+    return {
+      'grid-area': `${this.areas[index]}`
+    }
+  }
+  itemPrev: number = 0
+  clickTop: number | null = 0
+  guardarItem: string = ''
+  movePosition(index: number){
+    if(this.clickTop === null){
+      this.guardarItem = this.areas[this.itemPrev]
+      this.areas[this.itemPrev] = this.areas[index]
+      this.areas[index] = this.guardarItem
+      this.itemPrev = index
+    } else if(index !== this.clickTop){
+      this.guardarItem = this.areas[index]
+      this.areas[index] = this.areas[this.clickTop]
+      this.areas[this.clickTop] = this.guardarItem
+      this.itemPrev = index
+      this.clickTop = null
+    }
+  }
 }
