d7adc8a63d3ed13b740a37373d9fc72c36a07808
Kevin <dennisk.blas@gmail.com>
2024-10-26 15:38:29 -0500

    Estructura para las 3 secciones del navbar y verificacion de Rutas variables incorrectas para mostrar NotFound

---
+++ src/app/components/house/house.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { allHouse } from '../../data/houses';
+import { House } from '../../data/interfaces';
+@Component({
+  selector: 'app-house',
+  templateUrl: './house.component.html',
+  styleUrl: './house.component.scss'
+})
+export class HouseComponent implements OnInit{
+
+  houses: House[] = allHouse
+  indexHouse: number = 0
+  constructor(private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.selectOne()
+  }
+
+  selectOne(): void{
+    const houseSelected = this.activatedRoute.snapshot.paramMap.get('house')
+    const index = this.houses.findIndex(h => h.redirectHouse === houseSelected)
+    this.indexHouse = index
+  }
+
+  houseOK(): boolean{
+    const numbersHouses = [0, 1, 2, 3]
+    return numbersHouses.includes(this.indexHouse) ? true : false
+  }
+}
