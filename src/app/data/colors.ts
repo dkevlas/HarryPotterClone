30aa7d4878215401fd2b8c4bdb628afaeb7d91a6
Kevin <dennisk.blas@gmail.com>
2024-10-29 17:47:10 -0500

    Cambio automatico de colores

---
+++ src/app/data/colors.ts
@@ -0,0 +1,32 @@
+export interface Color {
+    primary: string,
+    secondary: string
+}
+const gryffindor: Color = {
+    primary: '#A50000',
+    secondary: '#FFC107'
+}
+const slytherin: Color = {
+    primary: '#2A623D',
+    secondary: '#C0C0C0'
+}
+const hufflepuff: Color = {
+    primary: '#F6EB61',
+    secondary: '#000000'
+}
+const ravenclaw: Color = {
+    primary: '#003DA5',
+    secondary: '#C0C0C0',
+}
+
+export const colorsHouse: Color[] = [
+    gryffindor,
+    slytherin,
+    hufflepuff,
+    ravenclaw,
+]
+
+export function changeColor(index: number){
+    document.documentElement.style.setProperty('--primary-color', colorsHouse[index].primary);
+    document.documentElement.style.setProperty('--secondary-color', colorsHouse[index].secondary);
+}
